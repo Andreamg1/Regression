fcb6459cbb4b88dab74f2a570b31a8bc06ba97ed
Andreamg1 <145478212+Andreamg1@users.noreply.github.com>
2023-10-04 19:46:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 
 async function runExample() {
 
+var x = new float32Array(1,11)
+  document.getElementById('box1').value;
+  document.getElementById('box2').value;
+  document.getElementById('box3').value;
+  document.getElementById('box4').value;
+  document.getElementById('box5').value;
+  document.getElementById('box6').value;
+  document.getElementById('box7').value;
+  document.getElementById('box8').value;
+  document.getElementById('box9').value;
+  document.getElementById('box10').value;
+  document.getElementById('box11').value;
 
+  let tensorx = newonnx.Tensor(x,'float32',[1,11])
 }
